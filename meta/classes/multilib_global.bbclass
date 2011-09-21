openembedded/openembedded-core

commit 329d864f9bbf94ad3aae8df43d63fe10e4237e4f

multilib: Remove recipe from multilib.conf that inherits allarch
Recipes like update-rc.d and qemu-config inherit "allarch", thus we
shouldn't add multilib BBCLASSEXTEND for them in multilib.conf.

Besides, we need to add multilib packages as the RPROVIDER contents for
those recipes, in order to avoid the NoProvider error when parsing.

[YOCTO #1471]

Signed-off-by: Dongxiao Xu <dongxiao.xu@intel.com>
Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

meta/classes/multilib_global.bbclass
@@ -7,11 +7,15 @@ python multilib_virtclass_handler_global () {
     if isinstance(e, bb.event.RecipeParsed) and not variant:
         if bb.data.inherits_class('kernel', e.data) or bb.data.inherits_class('module-base', e.data) or bb.data.inherits_class('allarch', e.data):
             origprovs = provs = e.data.getVar("PROVIDES", True)
+            rprovs = e.data.getVar("RPROVIDES", True)
             variants = (e.data.getVar("MULTILIB_VARIANTS", True) or "").split()
             for variant in variants:
                 provs = provs + " " + multilib_map_variable("PROVIDES", variant, e.data)
+                for pkg in e.data.getVar("PACKAGES", True).split():
+                    rprovs = rprovs + " " + variant + "-" + pkg
                 e.data.setVar("PROVIDES", origprovs)
             e.data.setVar("PROVIDES", provs)
+            e.data.setVar("RPROVIDES", rprovs)
 }
 
 addhandler multilib_virtclass_handler_global

meta/conf/multilib.conf
@@ -324,7 +324,6 @@ BBCLASSEXTEND_append_pn-psplash = " ${MULTILIBS}"
 BBCLASSEXTEND_append_pn-pth = " ${MULTILIBS}"
 BBCLASSEXTEND_append_pn-python-dbus = " ${MULTILIBS}"
 BBCLASSEXTEND_append_pn-python = " ${MULTILIBS}"
-BBCLASSEXTEND_append_pn-qemu-config = " ${MULTILIBS}"
 BBCLASSEXTEND_append_pn-qemugl = " ${MULTILIBS}"
 BBCLASSEXTEND_append_pn-qt4-x11-free = " ${MULTILIBS}"
 BBCLASSEXTEND_append_pn-quota = " ${MULTILIBS}"
@@ -394,7 +393,6 @@ BBCLASSEXTEND_append_pn-udev-extraconf = " ${MULTILIBS}"
 BBCLASSEXTEND_append_pn-udev = " ${MULTILIBS}"
 BBCLASSEXTEND_append_pn-unzip = " ${MULTILIBS}"
 BBCLASSEXTEND_append_pn-update-modules = " ${MULTILIBS}"
-BBCLASSEXTEND_append_pn-update-rc.d = " ${MULTILIBS}"
 BBCLASSEXTEND_append_pn-usbutils = " ${MULTILIBS}"
 BBCLASSEXTEND_append_pn-util-linux = " ${MULTILIBS}"
 BBCLASSEXTEND_append_pn-util-macros = " ${MULTILIBS}"

dhall55/poky-contrib

commit be1367ae4a7a8800fb0dc000f80ae083247fb8e0

multilib_global.bbclass: Fix non-multilib package provides
In non-multilib packages, configured in a multilib configuration we
need to adjust the system provides and rprovides to include the
virtual multilib variant.

This resolves a problem introduced in the
329d864f9bbf94ad3aae8df43d63fe10e4237e4f commit.  Where "allarch"
packages were suddenly providing all variants of an object.

Signed-off-by: Mark Hatle <mark.hatle@windriver.com>

meta/classes/multilib_global.bbclass
@@ -6,16 +6,33 @@ python multilib_virtclass_handler_global () {
 
     if isinstance(e, bb.event.RecipeParsed) and not variant:
         if bb.data.inherits_class('kernel', e.data) or bb.data.inherits_class('module-base', e.data) or bb.data.inherits_class('allarch', e.data):
-            origprovs = provs = e.data.getVar("PROVIDES", True)
-            rprovs = e.data.getVar("RPROVIDES", True)
             variants = (e.data.getVar("MULTILIB_VARIANTS", True) or "").split()
+
+            # Process PROVIDES
+            origprovs = provs = e.data.getVar("PROVIDES", True) or ""
             for variant in variants:
                 provs = provs + " " + multilib_map_variable("PROVIDES", variant, e.data)
-                for pkg in e.data.getVar("PACKAGES", True).split():
-                    rprovs = rprovs + " " + variant + "-" + pkg
+                # Reset to original value so next time around multilib_map_variable works properly
                 e.data.setVar("PROVIDES", origprovs)
             e.data.setVar("PROVIDES", provs)
+
+            # Process RPROVIDES
+            origrprovs = rprovs = e.data.getVar("RPROVIDES", True) or ""
+            for variant in variants:
+                rprovs = rprovs + " " + multilib_map_variable("RPROVIDES", variant, e.data)
+                # Reset to original value so next time around multilib_map_variable works properly
+                e.data.setVar("RPROVIDES", origrprovs)
             e.data.setVar("RPROVIDES", rprovs)
+
+	    # Process RPROVIDES_${PN}...
+            for pkg in (e.data.getVar("PACKAGES", True) or "").split():
+                origrprovs = rprovs = e.data.getVar("RPROVIDES_%s" % pkg, True) or ""
+                for variant in variants:
+                    rprovs = rprovs + " " + multilib_map_variable("RPROVIDES_%s" % pkg, variant, e.data)
+                    rprovs = rprovs + " " + variant + "-" + pkg
+                    # Reset to original value so next time around multilib_map_variable works properly
+                    e.data.setVar("RPROVIDES_%s" % pkg, origrprovs)
+                e.data.setVar("RPROVIDES_%s" % pkg, rprovs)
 }
 
 addhandler multilib_virtclass_handler_global
@@ -35,7 +52,7 @@ def multilib_extend_name(variant, name):
 def multilib_map_variable(varname, variant, d):
     var = d.getVar(varname, True)
     if not var:
-        return
+        return ""
     var = var.split()
     newvar = []
     for v in var: