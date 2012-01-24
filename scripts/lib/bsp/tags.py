8828961b248f020dbaf3da8632e86c06b7741189
Tom Zanussi <tom.zanussi@intel.com>
2012-01-24 00:29:02 -0600

    yocto-bsp: some constants
    Initial revision, some constants used by the templating engine for
    now.
    
    Signed-off-by: Tom Zanussi <tom.zanussi@intel.com>

---
+++ scripts/lib/bsp/tags.py
@@ -0,0 +1,37 @@
+# ex:ts=4:sw=4:sts=4:et
+# -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+#
+# Copyright 2012 Intel Corporation
+# Authored-by:  Tom Zanussi <tom.zanussi@intel.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+OPEN_TAG =    "{{"
+CLOSE_TAG =   "}}"
+ASSIGN_TAG =  "{{="
+INPUT_TAG =   "input"
+IF_TAG =      "if"
+
+INDENT_STR =  "    "
+
+BLANKLINE_STR = "of.write(\"\\n\")"
+NORMAL_START =  "of.write"
+OPEN_START =    "of = open"
+
+INPUT_TYPE_PROPERTY = "type"
+
+
+
+
+
